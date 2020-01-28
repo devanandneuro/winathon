1c2af63a6459eff2e8d611acc810cbfc35bc1ecc
devanandneuro <sdindi96@gmail.com>
2020-01-28 10:02:09 +0530

    changed the chart type more dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as FusionCharts from 'fusioncharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
  
   title = 'winathon';
   isActive = true;
-  
+  charttype:string;
+  datasource=null;
+  ifChartSelected=false;
+
   currChart:{id:number,chartType:string,page:string}[]=[];
   charts=[
       {id:1, chartType:'Bar chart',page:'column2d'},
       {id:2, chartType:'Line chart',page:'line'},
       {id:3, chartType:'Comparison chart',page:'mscolumn2d'}]
       ;
-  
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
+
+
+  constructor(private router: Router) { 
+
+    
   }
 
-  constructor(private router: Router) { }
 
   public onChange(event): void {
+    this.ifChartSelected=true;
     this.currChart = this.charts.filter(element=>element.id===event.value);
-    console.log(this.currChart[0].page);
-    this.router.navigate(['/app-'+this.currChart[0].page]);
+    //console.log(this.currChart[0].page);
+    //this.router.navigate(['/app-'+this.currChart[0].page]);
+    this.charttype=this.currChart[0].page;
+    this.datasource={
+      "chart": {
+          "caption": "SDWAN Product",
+          "subcaption": "Last 3 months",
+          "xaxisname": "Months",
+          "yaxisname": "Orders",
+          "theme": "fusion",
+          "showValues": "1",
+          "lineThickness": "2",
+      },
+      "data": [
+        {
+          "label": "October",
+          "value": "154",
+          "link": "newchart-xml-oct"
+      },
+      {
+          "label": "November",
+          "value": "123",
+          "link": "newchart-xml-nov"
+      },
+      {
+          "label": "December",
+          "value": "143",
+          "link": "newchart-xml-dec"
+      }
+      ]
+  }
   }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,27 +12,20 @@ import { FusionChartsModule } from 'angular-fusioncharts';
 import * as FusionCharts from 'fusioncharts';
 import * as charts from 'fusioncharts/fusioncharts.charts';
 import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
-import { Column2dComponent } from './column2d/column2d.component';
-import { LineComponent } from './line/line.component';
-import { Mscolumn2dComponent } from './mscolumn2d/mscolumn2d.component';
+
 import { RouterModule, Routes } from '@angular/router';
 
 // Pass the fusioncharts library and chart modules
 FusionChartsModule.fcRoot(FusionCharts, charts, FusionTheme);
 
 const appRoutes: Routes = [
-  { path: 'app-column2d', component: Column2dComponent},
-  { path: 'app-line', component: LineComponent},
-  { path: 'app-mscolumn2d', component: Mscolumn2dComponent},
 
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    Column2dComponent,
-    LineComponent,
-    Mscolumn2dComponent
+
   ],
   imports: [
     BrowserModule,
